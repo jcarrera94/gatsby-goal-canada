f2a0e212b69e819b368ebb383421ab7556ad074b
Juan Carrera <juankarrera@gmail.com>
2020-01-22 23:56:11 -0500

    add styles to header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from './header.module.scss'
+
 const Header = () => {
   return (
     <header>
       <h1>Goal Canada</h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link className={headerStyles.link} to="/">Home</Link></li>
+        <li><Link className={headerStyles.link} to="/about">About</Link></li>
+        <li><Link className={headerStyles.link} to="/services">Services</Link></li>
+        <li><Link className={headerStyles.link} to="/blog">Blog</Link></li>
+        <li><Link className={headerStyles.link} to="/contact">Contact</Link></li>
       </ul>
     </header>
   )
