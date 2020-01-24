5a1cae104508a653feaf565fd1f92d0c8cee9509
Juan Carrera <juankarrera@gmail.com>
2020-01-24 00:00:25 -0500

    add header style to header component

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ import headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Goal Canada</h1>
-      <ul>
-        <li><Link className={headerStyles.link} to="/">Home</Link></li>
-        <li><Link className={headerStyles.link} to="/about">About</Link></li>
-        <li><Link className={headerStyles.link} to="/services">Services</Link></li>
-        <li><Link className={headerStyles.link} to="/blog">Blog</Link></li>
-        <li><Link className={headerStyles.link} to="/contact">Contact</Link></li>
+    <header className={headerStyles.header}>
+      <h1 className={headerStyles.title}>Goal Canada</h1>
+      <ul className={headerStyles.navList}>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/services">Services</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
       </ul>
     </header>
   )
