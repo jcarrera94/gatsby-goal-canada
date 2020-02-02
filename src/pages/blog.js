203f39ae862f9471d95ca5ce31ea9184eaa3e9df
Juan Carrera <juankarrera@gmail.com>
2020-02-02 13:58:38 -0500

    render posts from remark files into blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Layout from '../components/layout';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const Blog = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+  `);
   return (
     <Layout>
       <h1>Blog</h1>
+      <ul>
+        {data.allMarkdownRemark.edges.map((edge, ix) => {
+          return (
+            <li key={ix}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>)
+        })}
+      </ul>
     </Layout>
   )
 }
