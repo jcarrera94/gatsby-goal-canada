4600ec063782bc0b72c923ee8ec0b09cc75e116a
Juan Carrera <juankarrera@gmail.com>
2020-02-04 15:44:31 -0500

    obtain markdown posts body and render to html elements on blog template component dynamically

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby';
 
-export const query = graphql`
+export  const query = graphql`
 query (
     $slug: String!
   ) {
@@ -22,10 +22,12 @@ query (
   }
 `
 
-const Blog = () => {
+const Blog = (props) => {
   return (
     <Layout>
-      This is the blog template.
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
     </Layout>
   )
 }
